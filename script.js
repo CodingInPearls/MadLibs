f11543fe3e8d85b470a49c05bc4c6d755005deb5
Christine B <99048641+CodingInPearls@users.noreply.github.com>
2022-07-22 04:15:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let noun = "La Croix Can";
+let adj = "smooth";
+let verb = "scrolled";
+let name = "Dottie";
+
+console.log("The " + noun + " looked " + adj + ", but I really wish " + name + " hadn't " + verb + " it.");
